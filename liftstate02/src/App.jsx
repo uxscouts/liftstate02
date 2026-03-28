4986831b90b2740a4392bc4f583b68767e6fb8ba
UX SCOUTS <admin@uxscouts.com>
2026-03-28 18:18:59 +0000

    first commit

---
+++ liftstate02/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
+
+
+function App() {
+
+  return (
+    <>
+      <div className="container">
+        <p>Something</p>
+      </div>
+    </>
+  )
+}
+
+export default App
